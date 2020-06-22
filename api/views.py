99b62f50dffdc066de75de03699723bb733fd7f4
luisfff29 <luisfff29@gmail.com>
2020-06-22 15:53:39 -0500

    add extra actions to viewset

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from ghostpost.models import GhostModel
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from api.serializers import GhostModelSerializer
 
 
@@ -7,3 +9,17 @@ from api.serializers import GhostModelSerializer
 class GhostModelViewSet(viewsets.ModelViewSet):
     queryset = GhostModel.objects.all()
     serializer_class = GhostModelSerializer
+
+    @action(detail=True, methods=['post'])
+    def up_vote(self, request, pk=None):
+        post = self.get_object()
+        post.up_vote += 1
+        post.save()
+        return Response({'status': 'up vote set'})
+
+    @action(detail=True, methods=['post'])
+    def down_vote(self, request, pk=None):
+        post = self.get_object()
+        post.down_vote += 1
+        post.save()
+        return Response({'status': 'down vote set'})
